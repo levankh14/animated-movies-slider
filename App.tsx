116c2c7223526b5a9871e06bef849ef852dfa716
levankh14 <levankh14@gmail.com>
2025-10-25 19:59:01 +0700

    boilerPlate

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import SearchComp from "./src/components/SearchComp";
 
 export default function App() {
   return (
-    <SafeAreaView edges={["top", "bottom"]} style={styles.container}>
-      <SearchComp />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView edges={["top", "bottom"]} style={styles.container}>
+        <SearchComp />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -16,6 +18,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#edf2f5",
     alignItems: "center",
     justifyContent: "flex-start",
-    paddingBottom: 24
+    paddingBottom: 24,
   },
 });
